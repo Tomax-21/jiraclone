c7a60e1456febf32d80ed3cbfae56e5ba4ab5a42
Tomax-21 <thom21fr@gmail.com>
2024-12-07 19:52:12 +0100

    Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex gap-4">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="muted">link</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="teritary">teritary</Button>
     </div>
   );
 }
